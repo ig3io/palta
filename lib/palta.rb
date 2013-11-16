503c6fc53d0f1a43ed6588c8ee22dfe4206f0a27
Ignacio Contreras <ignacio@ignacio.cat>
2013-11-16 21:34:11 +0100

    Improve socket debugging and add first attempts at JSON parsing

--- lib/palta.rb
+++ lib/palta.rb
@@ -1,8 +1,11 @@
 require "palta/version"
 require "socket"
+require "json"
 
 module Palta
 
+  # Generic Palta error to be thrown on module specific errors.
+  # It should not shadow any system error (i.e. ECONNREFUSED)
   class PaltaError < StandardError
   end
 
@@ -11,7 +14,7 @@ module Palta
     def initialize options = {}
       @host = options[:host] || "localhost"
       @port = options[:port] || 8888
-      @debug = options[:debug] || false
+      @debug = options[:debug] || options[:verbose] || false
       @dir = options[:dir] || "./.palta/data"
       @max_threads = options[:max_threads] || 8
       @threads = []
@@ -22,7 +25,11 @@ module Palta
     end
 
     def on_msg msg
-      send("on_#{msg[:type]}", msg)
+      send("on_#{msg[:type] || "default"}", msg)
+    end
+
+    def on_default msg
+      puts "default: #{msg}"
     end
 
     def start
@@ -31,7 +38,11 @@ module Palta
         @threads << Thread.new do
           loop do
             client = @server.accept
-            puts "thread #{i} read: #{client.recv(1024)}"
+            msg = client.recv(1024)
+            if @debug
+              puts "[Palta::Server] thread #{i} recv: #{msg}"
+            end
+            on_msg(JSON.parse(msg, :symbolize_keys => true))
           end
         end
       end
