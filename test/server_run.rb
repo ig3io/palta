b5538825b71d97b183478e74901d4167483b29b3
Ignacio Contreras <ignacio@ignacio.cat>
2013-11-17 11:11:02 +0100

    Add test_helper.rb and basic server_run script to be used while developing

---
+++ test/server_run.rb
@@ -0,0 +1,17 @@
+require "./test_helper"
+require "palta"
+
+s = Palta::Server.new({
+  :host => "localhost",
+  :port => 8888,
+  :debug => true
+})
+puts "[test] Starting server at #{s.host}:#{s.port}"
+s.start
+begin
+  loop do
+  end
+rescue Interrupt
+  puts "\n[test] Stopping server"
+  s.stop
+end
