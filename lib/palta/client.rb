e0e0e9fa71593f98db858408373a29dde3495684
Ignacio Contreras Pinilla <ignacio@ignacio.cat>
2013-11-25 23:41:29 +0100

    Add first Palta::Client implementation

--- lib/palta/client.rb
+++ lib/palta/client.rb
@@ -6,10 +6,17 @@ module Palta
 
   class Client
 
-    def initialize host, port, api_key
+    attr_reader :host, :port
+
+    def initialize host="localhost", port=8888
+      @host = host
+      @port = port
     end
 
     def send data
+      TCPSocket.open(@host, @port) do |s|
+        s.send data.to_json
+      end
     end
 
   end
